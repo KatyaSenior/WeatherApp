6dfb42624b90830a742d42faf39b393eef252288
Katya <katyasenior@gmail.com>
2024-04-16 15:12:39 +0100

    added weather conditions

--- client/app.js
+++ client/app.js
@@ -2,7 +2,7 @@ const temp = document.getElementById("temperature");
 
 async function fetchTemp() {
   const response = await fetch(
-    "https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&current=temperature_2m&hourly=temperature_2m&timezone=auto"
+    "https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&current=temperature_2m,is_day,rain,showers,snowfall&hourly=temperature_2m&timezone=auto"
   );
   const temperatures = await response.json();
   const currenttemp = temperatures.current.temperature_2m;
